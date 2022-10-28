b7d13023b70ba4bcea95cc71cfc2feef2e4ef7c6
Shinamin_97 <qnrlgkals@naver.com>
2022-10-28 16:49:17 +0900

    clean chache

--- project2/src/main/java/edu/kh/project/member/controller/MemberController.java
+++ project2/src/main/java/edu/kh/project/member/controller/MemberController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -299,6 +300,10 @@ public class MemberController {
 		}else { // 실패 시
 			path = referer;
 			message = "회원 가입 실패...";
+		
+			// 이전 페이지로 돌아갔을 때 입력했던 값을 같이 전달
+			inputMember.setMemberPw(null); // 비밀번호 삭제
+			ra.addFlashAttribute("tempMember",inputMember);
 		}
 		
 		ra.addFlashAttribute("message", message);
@@ -315,9 +320,35 @@ public class MemberController {
 	
 	
 	
+	/* 스피링 예외 처리 방법(3종류, 중복 사용 가능)
+	 * 
+	 * 1순위 : try-catch / throws 예외처리 구문
+	 * 		   -> 메서드 단위로 처리 
+	 * 
+	 * 2순위 : @ExceptionHandler 어노테이션
+	 * 		   -> 클래스 단위로 처리
+	 * 		   	- 하나의 컨트롤러에서 발생하는 예외를
+	 * 			  하나의 메서드에 모아서 처리
+	 * 
+	 * 3순위 : @ControllerAdvice 어노테이션
+	 * 		   -> 전역(웹 애플리케이션)에서 발생하는 예외를 모아서 처리
+	 * 		   -> 별도 클래스로 작성
+	 * 
+	 * */
 	
 	
-	
+	// MemberController에서 발생하는 모든 예외를 모아서 처리
+//	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(Exception e, Model model) {
+		
+		// 매개변수 Exception e : 발생한 예외 전달 받는 매개변수
+		e.printStackTrace();
+		
+		model.addAttribute("errorMessage", "회원 관련 서비스 이용 중 문제가 발생했습니다.");
+		model.addAttribute("e", e);
+		
+		return "common/error";
+	}
 	
 	
 	
